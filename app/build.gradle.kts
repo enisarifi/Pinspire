3b68ab1a9721c570e87a1456e3ce318277da5915
enisarifi <103397859+enisarifi@users.noreply.github.com>
2025-01-27 09:46:50 +0100

    Fixed the Material3.DayNight not regonzied bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,6 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.5.0")
     implementation("com.google.android.material:material:1.9.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.google.android.material:material:1.11.0-alpha05")
+
 }
